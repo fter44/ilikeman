5f731c47ad8bae4ed8c8a99289516289760caaeb
fter44 <fter44@naver.com>
2014-06-18 17:46:05 +0900

    Update DRAW_POS_MANAGER.lua

--- common/DRAW_POS_MANAGER.lua
+++ common/DRAW_POS_MANAGER.lua
@@ -7,7 +7,7 @@ local UPDATE_URL = "https://"..UPDATE_HOST..UPDATE_PATH
 
 function AutoupdaterMsg(msg) print("<font color=\"#6699ff\"><b>DRAW_POS_MANAGER:</b></font> <font color=\"#FFFFFF\">"..msg..".</font>") end
 if AUTOUPDATE then
-	local ServerData = GetWebResult(UPDATE_HOST, "/fter44/ilikeman/master/VersionFiles/DRAW_POS_MANAGER.version")
+	local ServerData = GetWebResult(UPDATE_HOST, "/fter44/ilikeman/master/VersionFiles/DRAW_POS_MANAGER.version".."?rand="..math.random(1,10000))
 	if ServerData then
 		ServerVersion = type(tonumber(ServerData)) == "number" and tonumber(ServerData) or nil
 		if ServerVersion then
