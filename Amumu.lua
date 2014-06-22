a34a0871a851520874fef5af5d63b35e0e84cbe2
fter44 <fter44@naver.com>
2014-06-22 19:57:26 +0900

    Update Amumu.lua

--- Amumu.lua
+++ Amumu.lua
@@ -2,7 +2,7 @@ if myHero.charName ~= "Amumu" then return end
 
 
 
-local version = "0.21"
+local version = "0.22"
 local SCRIPT_NAME = "Amumu"
 local AUTOUPDATE = true
 ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -25,7 +25,7 @@ if AUTOUPDATE then
 end
 
 local RequireI = Require("SourceLib")
-RequireI:Add("VPrediction", 	"https://raw.github.com/fter44/ilikeman/master/common/LEVEL.lua")
+RequireI:Add("VPrediction", 	"https://raw.github.com/fter44/ilikeman/master/common/VPrediction.lua")
 RequireI:Add("FTER_SOW", 		"https://raw.github.com/fter44/ilikeman/master/common/FTER_SOW.lua")
 RequireI:Add("DRAW_POS_MANAGER","https://raw.github.com/fter44/ilikeman/master/common/DRAW_POS_MANAGER.lua")
 RequireI:Add("ITEM_MANAGER", 	"https://raw.github.com/fter44/ilikeman/master/common/ITEM_MANAGER.lua")
